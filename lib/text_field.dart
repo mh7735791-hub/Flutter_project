4d417cdcd5d36fd327d8387a48f8ef3ddf1f55c4
MITU <mh7735791@gmail.com>
2026-07-08 16:01:22 +0600

    text_field

--- lib/text_field.dart
+++ lib/text_field.dart
@@ -11,91 +11,83 @@ class _TextFieldScreenState extends State<TextFieldScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Text Field"),centerTitle: true,),
-       body: Center(
-         child: Column(
-           children: [
-             Padding(
-               padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 15),
-               child: TextField(
-                 decoration: InputDecoration(
-                   labelText: "Name",
-                   prefixIcon: Icon(Icons.person,),
-                   border:OutlineInputBorder(
-                     borderRadius: BorderRadius.circular(50),
-                     borderSide: BorderSide(color: Colors.brown,),
-
-                   ),
-                   enabledBorder: OutlineInputBorder(
-                     borderRadius: BorderRadius.circular(70),
-                     borderSide: BorderSide(color: Colors.brown,),
-                   ),
-
-                 ),
-               ),
-             ),
-             Padding(
-               padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 15),
-               child: TextField(
-                 decoration: InputDecoration(
-                   labelText: "Email & Number",
-                   prefixIcon: Icon(Icons.email,),
-                   border:OutlineInputBorder(
-                     borderRadius: BorderRadius.circular(50),
-                     borderSide: BorderSide(color: Colors.brown,),
-
-                   ),
-                   enabledBorder: OutlineInputBorder(
-                     borderRadius: BorderRadius.circular(50),
-                     borderSide: BorderSide(color: Colors.brown,),
-                   ),
-
-                 ),
-               ),
-             ),
-             Padding(
-               padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 15),
-               child: TextField(
-                 decoration: InputDecoration(
-                   labelText: "Password",
-                   suffixIcon: Icon(Icons.remove_red_eye,),
-                   border:OutlineInputBorder(
-                     borderRadius: BorderRadius.circular(50),
-                     borderSide: BorderSide(color: Colors.brown,),
-
-                   ),
-                   enabledBorder: OutlineInputBorder(
-                     borderRadius: BorderRadius.circular(50),
-                     borderSide: BorderSide(color: Colors.brown,),
-                   ),
-
-                 ),
-               ),
-             ),
-             Padding(
-               padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 15),
-               child: TextField(
-                 decoration: InputDecoration(
-                   labelText: "Conform password",
-                   suffixIcon: Icon(Icons.remove_red_eye,),
-                   border:OutlineInputBorder(
-                     borderRadius: BorderRadius.circular(50),
-                     borderSide: BorderSide(color: Colors.brown,),
-
-                   ),
-                   enabledBorder: OutlineInputBorder(
-                     borderRadius: BorderRadius.circular(50),
-                     borderSide: BorderSide(color: Colors.brown,),
-                   ),
-                 ),
-               ),
-             ),
-             SizedBox(height: 15,),
-             ElevatedButton(onPressed: (){}, 
-                 child: Text("Submit"))
-           ],
-         ),
-       ),
+      appBar: AppBar(title: Text("Text Field"), centerTitle: true),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Name",
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: BorderSide(color: Colors.brown),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(70),
+                    borderSide: BorderSide(color: Colors.brown),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Email & Number",
+                  prefixIcon: Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: BorderSide(color: Colors.brown),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: BorderSide(color: Colors.brown),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  suffixIcon: Icon(Icons.remove_red_eye),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: BorderSide(color: Colors.brown),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: BorderSide(color: Colors.brown),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Conform password",
+                  suffixIcon: Icon(Icons.remove_red_eye),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: BorderSide(color: Colors.brown),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: BorderSide(color: Colors.brown),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 15),
+            ElevatedButton(onPressed: () {}, child: Text("Submit")),
+          ],
+        ),
+      ),
     );
   }
 }
